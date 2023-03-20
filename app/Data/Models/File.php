d6509b61b6b676aa80dcd4457f06e2ebfa156717
Nay Thu Khant <naythukhant644@gmail.com>
2023-03-20 18:46:44 +0630

    Lucid was replaced by laranex/lucid

--- app/Data/Models/File.php
+++ app/Data/Models/File.php
@@ -4,7 +4,6 @@ namespace App\Data\Models;
 
 use App\Traits\Attachable as AttachableTrait;
 use App\Traits\SnowflakeID;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
