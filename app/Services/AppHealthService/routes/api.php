ec3b2ecce7f705b1e5de60bd14e0c49ca21488b3
Nay Thu Khant <naythukhant644@gmail.com>
2023-03-24 14:39:11 +0630

    unnecessary resources removed from services

--- app/Services/AppHealthService/routes/api.php
+++ app/Services/AppHealthService/routes/api.php
@@ -1,6 +1,3 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Spatie\Health\Http\Controllers\HealthCheckJsonResultsController;
-
 Route::post('health', HealthCheckJsonResultsController::class);
--- app/Services/AppHealthService/routes/web.php
+++ app/Services/AppHealthService/routes/web.php
@@ -1,8 +1,4 @@
 <?php
 
-use App\Services\AppHealthService\Http\Controllers\SimpleHealthCheckController;
-use Illuminate\Support\Facades\Route;
-use Spatie\Health\Http\Controllers\HealthCheckResultsController;
-
 Route::get('health', SimpleHealthCheckController::class);
 Route::get('/', HealthCheckResultsController::class)->middleware('webBasicAuth');
